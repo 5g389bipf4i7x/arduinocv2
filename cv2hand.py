6567fdfa81f40c71afec8ca88953b56c4c84bfdf
whvas <s110324@whsh.tc.edu.tw>
2023-12-09 00:36:31 +0800

    Add cv2hand.py

--- cv2hand.py
+++ cv2hand.py
@@ -12,20 +12,28 @@ cap=cv2.VideoCapture(0)
 hands = mpHands.Hands()
 # min_tracking_confidence=0.5 追蹤狀態 數值低刷新慢
 while True:
-    ret,img=cap.read()
-    img = cv2.flip(cv2.resize(img,(400,300)), 1)
+    ret,detimg=cap.read()
+    detimg = cv2.flip(cv2.resize(detimg,(400,300)), 1)
     if ret:
-        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        imgRGB = cv2.cvtColor(detimg,cv2.COLOR_BGR2RGB)
         resule = hands.process(imgRGB)
-        # print(resule.multi_hand_landmarks)
         if resule.multi_hand_landmarks:
-            for handLms in resule.multi_hand_landmarks:
-                mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
-                for i,lm in enumerate(handLms.landmark):
-                    print(i , int(lm.x*img.shape[0]) , int(lm.y*img.shape[1]))
+                for handLms in resule.multi_hand_landmarks:
+                    mpDraw.draw_landmarks(detimg,handLms,mpHands.HAND_CONNECTIONS)
+                    for i,lm in enumerate(handLms.landmark):
+                        print(i , int(lm.x*detimg.shape[0]) , int(lm.y*detimg.shape[1]))
 
-        cv2.imshow('img',img)
+        imgray = cv2.cvtColor(detimg,cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(imgray, (5, 5), 0)
+        thresh = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+        contours,_ =cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt)> min_area 
+                             and cv2.contourArea(cnt)<max_area] #手部面積尚未被定義
+
+        cv2.imshow('Hand Contour Detection',detimg)
     
     key=cv2.waitKey(2)
     if key ==27:
         break
+cap.release
+cv2.destroyAllWindows
